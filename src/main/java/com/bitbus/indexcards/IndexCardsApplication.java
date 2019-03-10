578dd2c8376a54d4ff2f62df131377eed3c641b3
JayProck <jprochnow1@gmail.com>
2019-03-10 14:13:10 -0400

    Configure serialization strategy
    Disables Open-Session-In-View (OSIV) and registers Hibernate5 module to avoid serialization attempts against lazy-loaded entity properties

--- src/main/java/com/bitbus/indexcards/IndexCardsApplication.java
+++ src/main/java/com/bitbus/indexcards/IndexCardsApplication.java
@@ -2,12 +2,21 @@ package com.bitbus.indexcards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 
 @SpringBootApplication
 public class IndexCardsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IndexCardsApplication.class, args);
-	}
+    @Bean
+    public Module hibernateModule() {
+        return new Hibernate5Module();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndexCardsApplication.class, args);
+    }
 
 }
