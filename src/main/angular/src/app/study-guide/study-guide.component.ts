0517d488711acaba8397ed533bdf4da0ea512260
JayProck <jprochnow1@gmail.com>
2019-03-21 21:07:24 -0400

    Create basic index cards

--- src/main/angular/src/app/study-guide/study-guide.component.ts
+++ src/main/angular/src/app/study-guide/study-guide.component.ts
@@ -12,13 +12,36 @@ import { StudyGuideService } from '../core/services/study-guide.service';
 export class StudyGuideComponent implements OnInit {
 
   studyGuide: StudyGuide = {name: '', indexCards: []};
+  cardNum = 0;
+  showFront = true;
 
   constructor(private route: ActivatedRoute, private studyGuideService: StudyGuideService) { }
 
   ngOnInit() {
     const studyGuideId = this.route.snapshot.paramMap.get('studyGuideId');
-    this.studyGuideService.findStudyGuide(studyGuideId)
-      .subscribe(studyGuide => this.studyGuide = studyGuide);
+    this.studyGuideService.findStudyGuide(studyGuideId).subscribe(studyGuide => {
+      this.studyGuide = studyGuide;
+    });
+  }
+
+  onIndexCardClick() {
+    this.showFront = !this.showFront;
+  }
+
+  onPreviousClick() {
+    this.cardNum--;
+    if (this.cardNum < 0) {
+      this.cardNum = this.studyGuide.indexCards.length - 1;
+    }
+    this.showFront = true;
+  }
+
+  onNextClick() {
+    this.cardNum++;
+    if (this.cardNum >= this.studyGuide.indexCards.length) {
+      this.cardNum = 0;
+    }
+    this.showFront = true;
   }
 
 }
