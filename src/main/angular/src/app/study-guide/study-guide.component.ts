8eea0f45fb8e8af1fbd48e63f5dce24c3cbf1d23
Joe Prochnow <joe.prochnow@neovera.com>
2019-03-27 17:43:01 -0400

    Add index-card flip animation

--- src/main/angular/src/app/study-guide/study-guide.component.ts
+++ src/main/angular/src/app/study-guide/study-guide.component.ts
@@ -3,17 +3,77 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StudyGuide } from '../core/models/study-guide';
 import { StudyGuideService } from '../core/services/study-guide.service';
+import { transition, state, trigger, style, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'idx-study',
   templateUrl: './study-guide.component.html',
-  styleUrls: ['./study-guide.component.css']
+  styleUrls: ['./study-guide.component.css'],
+  animations: [
+    trigger('flip', [
+      state('back', style({
+        transform: 'rotateY(180deg)'
+      })),
+      transition('front => back', [
+        animate('0.5s 0s ease-out',
+          keyframes([
+            style({
+              transform: 'perspective(400px) rotateY(0deg)',
+              offset: 0
+            }),
+            style({
+              transform: 'perspective(400px) scale3d(1.1, 1.1, 1.1) rotateY(80deg)',
+              offset: 0.4
+            }),
+            style({
+              transform: 'perspective(400px) scale3d(1.2, 1.2, 1.2) rotateY(100deg)',
+              offset: 0.5
+            }),
+            style({
+              transform: 'perspective(400px) scale3d(0.95, 0.95, 0.95) rotateY(180deg)',
+              offset: 0.8
+            }),
+            style({
+              transform: 'perspective(400px) rotateY(180deg)',
+              offset: 1
+            })
+          ])
+        )
+      ]),
+      transition('back => front', [
+        animate('0.5s 0s ease-out',
+          keyframes([
+            style({
+              transform: 'perspective(400px) rotateY(-180deg)',
+              offset: 0
+            }),
+            style({
+              transform: 'perspective(400px) scale3d(1.1, 1.1, 1.1) rotateY(100deg)',
+              offset: 0.4
+            }),
+            style({
+              transform: 'perspective(400px) scale3d(1.2, 1.2, 1.2) rotateY(80deg)',
+              offset: 0.5
+            }),
+            style({
+              transform: 'perspective(400px) scale3d(0.95, 0.95, 0.95) rotateY(0deg)',
+              offset: 0.8
+            }),
+            style({
+              transform: 'perspective(400px) rotateY(0deg)',
+              offset: 1
+            })
+          ])
+        )
+      ]),
+    ])
+  ]
 })
 export class StudyGuideComponent implements OnInit {
 
   studyGuide: StudyGuide = {name: '', indexCards: []};
   cardNum = 0;
-  showFront = true;
+  visibleSide = 'front';
 
   constructor(private route: ActivatedRoute, private studyGuideService: StudyGuideService) { }
 
@@ -25,7 +85,11 @@ export class StudyGuideComponent implements OnInit {
   }
 
   onIndexCardClick() {
-    this.showFront = !this.showFront;
+    if (this.visibleSide == 'back') {
+      this.visibleSide = 'front';
+    } else {
+      this.visibleSide = 'back';
+    }
   }
 
   onPreviousClick() {
@@ -33,7 +97,7 @@ export class StudyGuideComponent implements OnInit {
     if (this.cardNum < 0) {
       this.cardNum = this.studyGuide.indexCards.length - 1;
     }
-    this.showFront = true;
+    this.visibleSide = 'front';
   }
 
   onNextClick() {
@@ -41,7 +105,7 @@ export class StudyGuideComponent implements OnInit {
     if (this.cardNum >= this.studyGuide.indexCards.length) {
       this.cardNum = 0;
     }
-    this.showFront = true;
+    this.visibleSide = 'front';
   }
 
 }
