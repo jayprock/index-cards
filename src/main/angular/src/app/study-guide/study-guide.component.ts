a96efea1b8e04742b5366bd345e98d4a0121fb10
JayProck <jprochnow1@gmail.com>
2019-04-02 05:35:32 -0400

    Improve index card flip animation

--- src/main/angular/src/app/study-guide/study-guide.component.ts
+++ src/main/angular/src/app/study-guide/study-guide.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 import { ActivatedRoute } from '@angular/router';
 import { StudyGuide } from '../core/models/study-guide';
 import { StudyGuideService } from '../core/services/study-guide.service';
-import { transition, state, trigger, style, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'idx-study',
@@ -15,52 +15,52 @@ import { transition, state, trigger, style, animate, keyframes } from '@angular/
         transform: 'rotateY(180deg)'
       })),
       transition('front => back', [
-        animate('0.5s 0s ease-out',
+        animate('0.8s 0s ease-out',
           keyframes([
             style({
-              transform: 'perspective(400px) rotateY(0deg)',
+              transform: 'perspective(2000px) rotateY(0deg)',
               offset: 0
             }),
             style({
-              transform: 'perspective(400px) scale3d(1.1, 1.1, 1.1) rotateY(80deg)',
+              transform: 'perspective(2000px) scale3d(1.05, 1.05, 1.05) rotateY(80deg)',
               offset: 0.4
             }),
             style({
-              transform: 'perspective(400px) scale3d(1.2, 1.2, 1.2) rotateY(100deg)',
+              transform: 'perspective(2000px) scale3d(1.06, 1.06, 1.06) rotateY(100deg)',
               offset: 0.5
             }),
             style({
-              transform: 'perspective(400px) scale3d(0.95, 0.95, 0.95) rotateY(180deg)',
+              transform: 'perspective(2000px) scale3d(1, 1, 1) rotateY(180deg)',
               offset: 0.8
             }),
             style({
-              transform: 'perspective(400px) rotateY(180deg)',
+              transform: 'perspective(2000px) rotateY(180deg)',
               offset: 1
             })
           ])
         )
       ]),
       transition('back => front', [
-        animate('0.5s 0s ease-out',
+        animate('0.8s 0s ease-out',
           keyframes([
             style({
-              transform: 'perspective(400px) rotateY(-180deg)',
+              transform: 'perspective(2000px) rotateY(-180deg)',
               offset: 0
             }),
             style({
-              transform: 'perspective(400px) scale3d(1.1, 1.1, 1.1) rotateY(100deg)',
+              transform: 'perspective(2000px) scale3d(1.05, 1.05, 1.05) rotateY(100deg)',
               offset: 0.4
             }),
             style({
-              transform: 'perspective(400px) scale3d(1.2, 1.2, 1.2) rotateY(80deg)',
+              transform: 'perspective(2000px) scale3d(1.06, 1.06, 1.06) rotateY(80deg)',
               offset: 0.5
             }),
             style({
-              transform: 'perspective(400px) scale3d(0.95, 0.95, 0.95) rotateY(0deg)',
+              transform: 'perspective(2000px) scale3d(1, 1, 1) rotateY(0deg)',
               offset: 0.8
             }),
             style({
-              transform: 'perspective(400px) rotateY(0deg)',
+              transform: 'perspective(2000px) rotateY(0deg)',
               offset: 1
             })
           ])
