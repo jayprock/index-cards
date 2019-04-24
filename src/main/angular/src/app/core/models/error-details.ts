ec1367bf015fddf912d083663b3185d3318c0a14
jayprock <jprochnow1@gmail.com>
2019-04-24 17:25:22 -0400

    Validate create studyguide on client

---
+++ src/main/angular/src/app/core/models/error-details.ts
@@ -0,0 +1,5 @@
+export interface ErrorDetails {
+
+    serverError?: boolean;
+    message?: string;
+}
--- src/main/angular/src/app/create/create.component.ts
+++ src/main/angular/src/app/create/create.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { IndexCard } from '../core/models/index-card';
 import { Router } from '@angular/router';
 import { StudyGuide } from '../core/models/study-guide';
 import { StudyGuideService } from '../core/services/study-guide.service';
+import { NgForm } from '@angular/forms';
+import { ErrorDetails } from '../core/models/error-details';
 
 @Component({
   selector: 'idx-create',
@@ -11,6 +13,8 @@ import { StudyGuideService } from '../core/services/study-guide.service';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+
+  @ViewChild('createForm') createForm: NgForm;
 
   studyGuide: StudyGuide = {
     name: "",
@@ -20,7 +24,7 @@ export class CreateComponent implements OnInit {
     ]
   };
 
-  submitError = false;
+  error: ErrorDetails = { serverError: false, message: null};
 
   constructor(private studyGuideService: StudyGuideService, private router: Router) { }
 
@@ -28,12 +32,18 @@ export class CreateComponent implements OnInit {
   }
 
   onSubmit() {
-    this.studyGuideService.createStudyGuide(this.studyGuide).subscribe(result => {
-      this.submitError = false;
-      this.router.navigateByUrl("/" + result.studyGuideId);
-    }, error => {
-      this.submitError = true;
-    });
+    if (this.createForm.form.valid) {
+      this.studyGuideService.createStudyGuide(this.studyGuide).subscribe(result => {
+        this.error = {};
+        this.router.navigateByUrl("/" + result.studyGuideId);
+      }, error => {
+        this.error.serverError = true;
+        this.error.message = "The request could not be completed due to a server error.";
+      });
+    } else {
+      this.error.serverError = false;
+      this.error.message = "Please enter all required fields and try again."
+    }
   }
 
   addFlashCard() {
