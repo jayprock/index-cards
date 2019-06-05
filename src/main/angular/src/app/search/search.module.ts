86a2f49e40b8a04506dcc3a5013e363809a16b99
JayProck <jprochnow1@gmail.com>
2019-06-05 13:54:49 -0400

    Add initial search page - frontend only

--- src/main/angular/src/app/search/search.module.ts
+++ src/main/angular/src/app/search/search.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { SearchRoutingModule } from './search-routing.module';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { SearchComponent } from './search.component';
+import { SearchRoutingModule } from './search-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SearchComponent],
   imports: [
     CommonModule,
-    SearchRoutingModule
+    SearchRoutingModule,
+    SharedModule,
+    FormsModule
   ]
 })
 export class SearchModule { }
