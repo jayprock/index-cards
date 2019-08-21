f5942bbd0ba613c0e31cc37f95402bd33fc62735
JayProck <jprochnow1@gmail.com>
2019-08-21 06:43:59 -0400

    Add login form

--- src/main/angular/src/app/login/login.component.ts
+++ src/main/angular/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'idx-login',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      login: [''],
+      password: ['']
+    });
   }
 
 }
