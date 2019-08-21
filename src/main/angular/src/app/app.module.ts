ef7cd53dd55ad6a95efc9ee331a1d207d56660d8
JayProck <jprochnow1@gmail.com>
2019-08-21 06:31:37 -0400

    Create login component & routing

--- src/main/angular/src/app/app.module.ts
+++ src/main/angular/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
 import { CreateModule } from './create/create.module';
 import { HomeModule } from './home/home.module';
+import { LoginModule } from './login/login.module';
 import { NgModule } from '@angular/core';
 import { RegisterModule } from './register/register.module';
 import { SearchModule } from './search/search.module';
@@ -23,7 +24,8 @@ import { StudyGuideModule } from './study-guide/study-guide.module';
     SearchModule,
     CreateModule,
     RegisterModule,
-    StudyGuideModule, // must be last
+    LoginModule,
+    StudyGuideModule, // must be last based on routing path
   ],
   providers: [],
   bootstrap: [AppComponent]
